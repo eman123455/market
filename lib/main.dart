6d5fe22d5538444cb97004fdd68abb6c289ac169
Eman Kamel <kamele432@gmail.com>
2025-11-30 06:00:29 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MarketApp());
+}
+
+class MarketApp extends StatelessWidget {
+  const MarketApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+    
+    );
+  }
+}
