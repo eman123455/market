9c29a380f7d8513e3647135a77e4e7a0ae0257f5
Eman Kamel <kamele432@gmail.com>
2025-12-05 12:12:55 +0200

    update routes file

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -2,7 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:market/features/login/presentation/views/login_view.dart';
 
 class AppRoutes {
-  static const String kLoginView = '/LoginView';
+  static const String kLoginView = '/';
 
   static GoRouter routes = GoRouter(
     routes:[
