0be39cdb954319b250b9048704a2ec2118274566
Eman Kamel <kamele432@gmail.com>
2025-12-06 01:31:53 +0200

    add on tap fun to custom text button

--- lib/features/login/presentation/widgets/custom_text_button.dart
+++ lib/features/login/presentation/widgets/custom_text_button.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomTextButton extends StatelessWidget {
-  const CustomTextButton({super.key, required this.textColor,required this.text});
+  const CustomTextButton({super.key,
+   required this.textColor,
+   required this.text,
+    required this.onTap});
   final Color textColor;
   final String text;
+  final void  Function() onTap;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
+      onTap: onTap,
       child: Text(
         text,
         style: TextStyle(
