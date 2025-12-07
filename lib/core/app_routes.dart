cbdc70c99ec23a534a8410038fce2d4dde4e5dde
Eman Kamel <kamele432@gmail.com>
2025-12-07 20:57:44 +0200

    add forgot route

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,10 +1,12 @@
 import 'package:go_router/go_router.dart';
+import 'package:market/features/forgot_password/presentation/forgot_password_view.dart';
 import 'package:market/features/login/presentation/views/login_view.dart';
 import 'package:market/features/sign%20up/presentation/views/sign_up_view.dart';
 
 class AppRoutes {
   static const String kLoginView = '/';
   static const String kSignUp = '/SignUpView';
+  static const String kForgotPasswordView = '/ForgotPasswordView';
 
   static GoRouter routes = GoRouter(
     routes:[
@@ -15,6 +17,10 @@ class AppRoutes {
     GoRoute(
       path: kSignUp,
       builder: (context, state) =>  SignUpView(),
+      ),
+    GoRoute(
+      path: kForgotPasswordView,
+      builder: (context, state) =>  ForgotPasswordView(),
       ),
    ]
    );
