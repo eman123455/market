e29cc3ff81ce7b7a85de39a03f63439cb9b6ad70
Eman Kamel <kamele432@gmail.com>
2025-12-07 20:57:29 +0200

    add on tap attribute to custom btn

--- lib/core/reusable_widgets/custom_button.dart
+++ lib/core/reusable_widgets/custom_button.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:market/core/app_colors.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key ,required this.buttonText});
+  const CustomButton({super.key ,required this.buttonText, required this.onTap});
   final String buttonText;
+  final void  Function() onTap;
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed:onTap,
         style: ElevatedButton.styleFrom(
           backgroundColor: AppColors.kPrimaryColor,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
