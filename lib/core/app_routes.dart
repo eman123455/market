142c77ec922e86b6ff4ad7643fea18545ad6cf3e
Eman Kamel <kamele432@gmail.com>
2025-12-07 18:20:54 +0200

    add sign up route

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,14 +1,20 @@
 import 'package:go_router/go_router.dart';
 import 'package:market/features/login/presentation/views/login_view.dart';
+import 'package:market/features/sign%20up/presentation/views/sign_up.dart';
 
 class AppRoutes {
   static const String kLoginView = '/';
+  static const String kSignUp = '/SignUp';
 
   static GoRouter routes = GoRouter(
     routes:[
     GoRoute(
       path: kLoginView,
       builder: (context, state) =>  LoginView(),
+      ),
+    GoRoute(
+      path: kSignUp,
+      builder: (context, state) =>  SignUp(),
       ),
    ]
    );
