f35e0d5966cca87e16273df3a63d37c211084a73
Eman Kamel <kamele432@gmail.com>
2025-12-07 17:24:18 +0200

    create login view

--- lib/features/login/presentation/views/login_view.dart
+++ lib/features/login/presentation/views/login_view.dart
@@ -1,10 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:market/core/app_colors.dart';
+import 'package:market/features/login/presentation/widgets/custom_button.dart';
+import 'package:market/features/login/presentation/widgets/custom_text_button.dart';
+import 'package:market/features/login/presentation/widgets/custom_text_field.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
-class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+class LoginView extends StatefulWidget {
+  LoginView({super.key});
 
   @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final TextEditingController emailController = TextEditingController();
+
+  final TextEditingController passController = TextEditingController();
+  bool obscureTextdecision = true;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 40),
+        child: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              children: [
+                Image.asset(
+                  'lib/assets/283e86d4-6cfc-4e55-988b-a1d4ae5b7094.png',
+                  width: 200,
+                  height: 200,
+                ),
+
+                Form(
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        hint: 'email',
+                        controller: emailController,
+                        inputType: TextInputType.emailAddress,
+                      ),
+                      const SizedBox(height: 15),
+                      CustomTextField(
+                        hint: 'password',
+                        controller: passController,
+                        inputType: TextInputType.number,
+                        obscureText: obscureTextdecision,
+                        sufficon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              obscureTextdecision = ! obscureTextdecision;
+                            });
+                          },
+                          icon: obscureTextdecision?Icon(Icons.visibility):Icon(Icons.visibility_off),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Align(
+                          alignment: Alignment.topRight,
+                          child: CustomTextButton(
+                            onTap: () {},
+                            textColor: AppColors.kPrimaryColor,
+                            text: 'Forgot Password ?',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      CustomButton(buttonText: 'Sign In'),
+                      SizedBox(height: 15),
+                      SizedBox(
+                        width: double.infinity,
+                        child: SignInButton(
+                          Buttons.google,
+                          padding: EdgeInsets.all(10),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          text: "Sign up with Google",
+                          textStyle: TextStyle(fontSize: 18),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Not a member yet? ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    CustomTextButton(
+                      onTap: () {},
+                      textColor: Colors.orange,
+                      text: 'Sign Up',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
