c945975d6cfdcd3b86e8cced135cde71c6703539
Eman Kamel <kamele432@gmail.com>
2025-12-10 17:46:45 +0200

    create list of icons with name of icons

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,14 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:market/features/favorite/presentation/favorite.dart';
 import 'package:market/features/home/presentation/views/home_body.dart';
+import 'package:market/features/home/presentation/widgets/category.dart';
 import 'package:market/features/profile/presentation/views/profile.dart';
 import 'package:market/features/store/presentation/views/store.dart';
 
 class Constants {
-  static  List<Widget> views = [
-    HomeBody(),
-    Store(),
-    Favorite(),
-    Profile(),
+  static List<Widget> views = [HomeBody(), Store(), Favorite(), Profile()];
+  static List<Categoryss> popularCategory = [
+    Categoryss(icon: Icons.sports, categoryName: 'Sports'),
+    Categoryss(icon: Icons.computer, categoryName: 'Electroics'),
+    Categoryss(icon: Icons.book, categoryName: 'Sports'),
+    Categoryss(icon: Icons.games, categoryName: 'Games'),
+    Categoryss(icon: Icons.bike_scooter, categoryName: 'Bikes'),
   ];
 }
