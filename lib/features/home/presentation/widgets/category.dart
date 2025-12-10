1750ed8818fe711b4625bc518cc99feacc3516e7
Eman Kamel <kamele432@gmail.com>
2025-12-10 17:45:53 +0200

    create category class

---
+++ lib/features/home/presentation/widgets/category.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Categoryss {
+  final IconData icon;
+  final  String categoryName;
+
+  Categoryss({required this.icon, required this.categoryName});
+}
