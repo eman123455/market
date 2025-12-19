061c67336a593376b322eedf4ba3a695c02ce4bc
Eman Kamel <kamele432@gmail.com>
2025-12-19 11:11:49 +0200

    finish store view ui

--- lib/features/store/presentation/views/store.dart
+++ lib/features/store/presentation/views/store.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:market/core/components/custom_search_field.dart';
+import 'package:market/core/components/product_List.dart';
 
 class Store extends StatelessWidget {
-  const Store({super.key});
-
+  Store({super.key});
+  final TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      child: Text('store'),
+    return ListView(
+      children: [
+        Center(
+          child: Text(
+            'Welcome to Shoplio',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(height: 10,),
+        CustomSearchField(searchControll: controller),
+        const SizedBox(height: 10,),
+        ProductList(),
+      ],
     );
   }
 }
