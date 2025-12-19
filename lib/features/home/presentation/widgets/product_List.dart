e7a9efaf75bbdf521abc4d0f2dd20dd37efab896
Eman Kamel <kamele432@gmail.com>
2025-12-19 10:56:05 +0200

    create product list

---
+++ lib/features/home/presentation/widgets/product_List.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:market/features/home/presentation/widgets/product_card.dart';
+
+class ProductList extends StatelessWidget {
+  const ProductList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(),
+          child: ProductCard(),
+        );
+      },
+    );
+  }
+}
