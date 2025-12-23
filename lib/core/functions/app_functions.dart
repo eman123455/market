060d24fcf41fbaad08f0a3aefd19016552fe0960
Eman Kamel <kamele432@gmail.com>
2025-12-23 05:02:34 +0200

    create open whats fun

---
+++ lib/core/functions/app_functions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:market/core/app_colors.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AppFunctions {
+  static void openWhatsapp() async {
+    final Uri url = Uri.parse('https://wa.me/201003286729');
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      SnackBar(
+        backgroundColor: AppColors.kBlackColor,
+        content: Text('Cann\'t open whatsApp')
+        );
+    }
+  }
+}
