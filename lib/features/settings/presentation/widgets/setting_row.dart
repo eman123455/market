9a35abb3120774a1bd8a087bdf5ce9d8da07c342
Eman Kamel <kamele432@gmail.com>
2025-12-23 05:01:48 +0200

    creat setting row

---
+++ lib/features/settings/presentation/widgets/setting_row.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:market/core/app_colors.dart';
+
+class SettingRow extends StatelessWidget {
+  const SettingRow({super.key, required this.settingName, required this.tap, required this.icon});
+  final String settingName;
+  final IconData icon;
+  final void Function() tap;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(icon, size: 28, color: AppColors.kPrimaryColor),
+        SizedBox(width: 8),
+        Text(settingName, style: TextStyle(fontSize: 16)),
+        Spacer(),
+        IconButton(
+          onPressed: tap,
+          icon: Icon(Icons.arrow_forward_ios, color: AppColors.kBlackColor),
+        ),
+        SizedBox(height: 30),
+      ],
+    );
+  }
+}
