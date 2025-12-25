82169ef4b44e7413897f144a0cf26e924614df47
Eman Kamel <kamele432@gmail.com>
2025-12-25 03:05:12 +0200

    edit custom text field

--- lib/core/components/custom_text_field.dart
+++ lib/core/components/custom_text_field.dart
@@ -8,13 +8,15 @@ class CustomTextField extends StatelessWidget {
     required this.controller,
     required this.inputType,
     this.obscureText = false,
-    this.sufficon
+    this.sufficon,
+    this.labelText=''
   });
   final TextEditingController controller;
   final String hint;
   final TextInputType inputType;
   final bool obscureText;
   final Widget? sufficon;
+  final String labelText;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
@@ -29,16 +31,18 @@ class CustomTextField extends StatelessWidget {
       keyboardType: inputType,
       decoration: InputDecoration(
         suffixIcon: sufficon,
-        hintText: '$hint',
+        hintText: hint,
+        label: Text(labelText,
+        style: TextStyle(
+          color: AppColors.kGreyColor
+        ),
+        ),
         border: OutlineInputBorder(
-          borderSide: BorderSide(color: AppColors.kPrimaryColor,),
+          borderSide: BorderSide(color: AppColors.kPrimaryColor),
           borderRadius: BorderRadius.circular(8),
         ),
         enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: AppColors.kGreyColor,
-            width: 2
-            ),
+          borderSide: BorderSide(color: AppColors.kGreyColor, width: 2),
           borderRadius: BorderRadius.circular(8),
         ),
         focusedBorder: OutlineInputBorder(
