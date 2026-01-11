cfad9719ebd25bd9bb798c2348aa5bda9ed20ba6
Eman Kamel <kamele432@gmail.com>
2026-01-11 18:07:10 +0200

    update app colors

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color kPrimaryColor = Color(0xFF0568DF);
+  static const Color kPrimaryColor = Color(0xFF181F6F);
   static const Color kScaffoldColor = Color(0xffEEEEEE);
   static const Color kBordersideColor = Color(0xffE1E1E1);
   static const Color kWhiteColor = Color(0xFFffffff);
