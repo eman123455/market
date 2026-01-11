deae43a81f4ce7c065bf8c9cc9266ca325172ac6
Eman Kamel <kamele432@gmail.com>
2026-01-11 18:14:07 +0200

    create profile page

---
+++ lib/features/settings/presentation/views/user_profile.dart
@@ -0,0 +1,92 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:market/core/app_colors.dart';
+import 'package:market/core/components/custom_button.dart';
+import 'package:market/core/components/custom_text_field.dart';
+
+class UserProfile extends StatelessWidget {
+ UserProfile({super.key});
+  final TextEditingController firstnameController= TextEditingController();
+  final TextEditingController lastnameController= TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:  AppColors.kScaffoldColor,
+        title: Text(
+          'User Profile',
+          style: TextStyle(color: AppColors.kPrimaryColor,fontWeight: FontWeight.bold),
+        ),
+        iconTheme: IconThemeData(color: AppColors.kPrimaryColor),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 70, right: 10,left: 10),
+        child: Form(
+          child: ListView(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage: CachedNetworkImageProvider(
+                          'https://images.unsplash.com/photo-1542291026-7eec264c27ff',
+                        ),
+                      ),
+                      Positioned(
+                        bottom: -4,
+                        right: -4,
+                        child: CircleAvatar(
+                          radius: 16,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.camera_alt,
+                            size: 18,
+                            color: AppColors.kPrimaryColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 40,),
+              CustomTextField(
+                hint: 'First name',
+                labelText: 'First name',
+                controller: firstnameController,
+                inputType: TextInputType.name,
+                ),
+              SizedBox(height: 18,),
+              CustomTextField(
+                hint: 'Last name',
+                labelText: 'last name',
+                controller: lastnameController,
+                inputType: TextInputType.name,
+                ),
+              SizedBox(height: 18,),
+              CustomTextField(
+                hint: 'E-mail',
+                labelText: 'E-mail',
+                controller: lastnameController,
+                inputType: TextInputType.name,
+                ),
+              SizedBox(height: 18,),
+              CustomButton(
+                buttonText:'UPDATE',
+                 onTap: (){
+        
+                 }
+                 )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
