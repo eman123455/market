9a1930672b1fd4450fde91f03fa686b89a27c267
Eman Kamel <kamele432@gmail.com>
2026-01-13 14:10:11 +0200

    initialize supabase in the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:market/core/app_colors.dart';
 import 'package:market/core/app_routes.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MarketApp());
 }
 
@@ -13,9 +21,7 @@ class MarketApp extends StatelessWidget {
     return MaterialApp.router(
       title: 'our market',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.kScaffoldColor
-      ),
+      theme: ThemeData(scaffoldBackgroundColor: AppColors.kScaffoldColor),
       routerConfig: AppRoutes.routes,
     );
   }
